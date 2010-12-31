10d53f5618d34c18ea467183c86c92b6dea1d1ed
Werring <thomwerring@gmail>
2010-12-31 13:51:48 +0100

    added suspend posibilities

--- plugin_team.rb
+++ plugin_team.rb
@@ -5,6 +5,9 @@ class Teamleden
   match /access (.+)/i , method: :access
   match /addhelper (.+)/i , method: :addHelper
   match /addadmin (.+)/i , method: :addAdmin
+  match /suspend (.+)/i , method: :suspend
+  match /unsuspend (.+)/i , method: :unsuspend
+  match /updateTeam/i , method: :updateTeam
   def initialize(*args)
     super
     @team = nil
@@ -69,7 +72,7 @@ class Teamleden
     end
   end
   def addAdmin(m, nick)
-    return unless admin(m.user)
+    return unless @team.admin(m.user)
     newAdmin = User(nick)
     alvl = aLevel(newAdmin)
     case alvl
@@ -93,6 +96,64 @@ class Teamleden
     end
   end
   
+  def suspend(m,nick)
+    return unless @team.admin(m.user)
+    suspended = User(nick)
+    alvl = aLevel(suspended)
+    case alvl
+    when "no"
+	 m.user.send "Can't suspend #{nick} (#{suspended.authname}), user doesn't have access"
+	 return
+    when "suspended"
+	 m.user.send "Can't suspend #{nick} (#{suspended.authname}), user is already suspended"
+	 return
+    end
+    if suspended.authed?
+      addedToDb = suspendTeamler(suspended,1)
+	 if addedToDb["error"]
+	   m.user.send "Couldn't suspend #{nick}, Error: #{addedToDb['errormsg']}"
+	 else
+	   m.user.send "Suspended #{nick} (#{suspended.authname})"
+	   @bot.raw "CS suspend *#{suspended.authname}"
+	 end
+    else
+	 m.user.send "No such nick #{nick}... try again please"
+    end
+  end
+  
+  def unsuspend(m,nick)
+    return unless @team.admin(m.user)
+    suspended = User(nick)
+    alvl = aLevel(suspended)
+    case alvl
+    when "no"
+	 m.user.send "Can't unsuspend #{nick} (#{suspended.authname}), user doesn't have access"
+	 return
+    when "admin"
+	 m.user.send "Can't unsuspend #{nick} (#{suspended.authname}), user isn't suspended"
+	 return
+    when "helper"
+	 m.user.send "Can't unsuspend #{nick} (#{suspended.authname}), user isn't suspended"
+  	 return
+    end
+    if suspended.authed?
+      addedToDb = suspendTeamler(suspended,0)
+	 if addedToDb["error"]
+	   m.user.send "Couldn't unsuspend #{nick}, Error: #{addedToDb['errormsg']}"
+	 else
+	   m.user.send "Unsuspended #{nick} (#{suspended.authname})"
+	   @bot.raw "CS unsuspend *#{suspended.authname}"
+	 end
+    else
+	 m.user.send "No such nick #{nick}... try again please"
+    end
+  end
+  
+  def updateTeam(m)
+    @team.createTeam
+    m.reply "Updated team database"
+  end
+  
   def addTeamlerToDb(user,access)
     returnVal = {'error'=>false}
     begin
@@ -105,8 +166,17 @@ class Teamleden
     return returnVal
   end
   
-  def suspendTeamler(user)
-    
+  def suspendTeamler(user,susp)
+    susp ||= 0
+    returnVal = {'error'=>false}
+    begin
+	 @mysql.query "UPDATE `team` SET `suspended`=#{susp} WHERE `auth`='#{user.authname}'"
+	 rescue Mysql::Error => e
+	   returnVal = {'error'=>true,'errormsg'=>e.error}
+	   puts returnVal
+	 ensure
+    end
+    return returnVal
   end
 
 
