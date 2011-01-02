18e8be04201a462ef69ab5c49983a18194aca294
Werring <thomwerring@gmail>
2011-01-02 16:43:59 +0100

    added commandlist

--- plugin_commands.rb
+++ plugin_commands.rb
@@ -2,6 +2,7 @@ class Commands
   include Cinch::Plugin
   prefix ">"
   match /(.+)(?: (.+))?/i , method: :command
+  match /commands/ , method: :commands
   #*/
   def initialize(*args)
     super
@@ -15,14 +16,30 @@ class Commands
     @commands = ["autovoice",
 			  "join","part","quit",
 			  "myaccess","access", "addhelper","addadmin","suspend","unsuspend",
-			  "adduser"
+			  "adduser",
+			  "commands"
 			  ]
   end
   
   def command(m,command)
     args = command.split(" ")
-    return if @commands.include?(args[0]) || @team.helper(m.user)!=true
+    return if @commands.include?(args[0]) || !@team.helper(m.user)
     m.user.send "command #{args[0]} is not known"
+  end
+  
+  def commands(m)
+    commandos = []
+    m.user.send "I know the following commands:"
+    @commands.each do |commando|
+	  commandos.push("#{commando}")
+	  if commandos.length > 4
+	    m.user.send commandos.join(", ")
+	    commandos = []
+	  end
+    end
+    if commandos.length>0
+	 m.user.send commandos.join(", ")
+    end
   end
 
 
